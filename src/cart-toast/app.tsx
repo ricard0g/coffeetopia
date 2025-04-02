341b302fa702c73d4a504ed95794a61d1c2449ab
ricard0g <ricardoguzman444@gmail.com>
2025-04-02 21:53:14 +0200

    feat(progress-bar): add style, functionality for adding and removing items, based on price some tiers are checked off. Problems with the 20% off because minPrice and maxPrice are different from the others

--- src/cart-toast/app.tsx
+++ src/cart-toast/app.tsx
@@ -1,11 +1,122 @@
 import { useEffect, useState, useCallback } from 'preact/hooks';
 
+const RewardTier = ({
+  tierLevel,
+  minPrice,
+  maxPrice,
+  cartTotalPrice,
+}: {
+  tierLevel: string;
+  minPrice: number;
+  maxPrice: number;
+  cartTotalPrice: number;
+}) => {
+  return (
+    <div className={`cart-toast__reward-tier`}>
+      <div className={`cart-toast__reward-tier-icon-wrapper`}>
+        <div className={`cart-toast__reward-tier-icon`}>
+          <svg
+            width="15px"
+            height="15px"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            id="discount"
+            class="icon glyph"
+            fill="#ffffff"
+          >
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+            <g id="SVGRepo_iconCarrier">
+              <path
+                d="M21.59,9.83A4.21,4.21,0,0,1,21.17,9,4.73,4.73,0,0,1,21,8.07a4.19,4.19,0,0,0-.64-2.16,4.15,4.15,0,0,0-1.87-1.28,4.36,4.36,0,0,1-.84-.43A4.55,4.55,0,0,1,17,3.54a4.29,4.29,0,0,0-1.81-1.4A4.19,4.19,0,0,0,13,2.21a4.24,4.24,0,0,1-1.94,0A4.19,4.19,0,0,0,8.8,2.14,4.29,4.29,0,0,0,7,3.54a4.55,4.55,0,0,1-.66.66,4.36,4.36,0,0,1-.84.43A4.15,4.15,0,0,0,3.62,5.91,4.19,4.19,0,0,0,3,8.07,4.73,4.73,0,0,1,2.83,9a4.21,4.21,0,0,1-.42.81A4.3,4.3,0,0,0,1.64,12a4.3,4.3,0,0,0,.77,2.17,4.21,4.21,0,0,1,.42.81,4.73,4.73,0,0,1,.15.95,4.19,4.19,0,0,0,.64,2.16,4.15,4.15,0,0,0,1.87,1.28,4.36,4.36,0,0,1,.84.43,4.55,4.55,0,0,1,.66.66,4.29,4.29,0,0,0,1.81,1.4,2.91,2.91,0,0,0,.87.13,6,6,0,0,0,1.36-.2,4.24,4.24,0,0,1,1.94,0,4.19,4.19,0,0,0,2.23.07A4.29,4.29,0,0,0,17,20.46a4.55,4.55,0,0,1,.66-.66,4.36,4.36,0,0,1,.84-.43,4.15,4.15,0,0,0,1.87-1.28A4.19,4.19,0,0,0,21,15.93a4.73,4.73,0,0,1,.15-.95,4.21,4.21,0,0,1,.42-.81A4.3,4.3,0,0,0,22.36,12,4.3,4.3,0,0,0,21.59,9.83ZM9.5,8A1.5,1.5,0,1,1,8,9.5,1.5,1.5,0,0,1,9.5,8Zm5,8A1.5,1.5,0,1,1,16,14.5,1.5,1.5,0,0,1,14.5,16Zm1.21-6.29-6,6a1,1,0,0,1-1.42,0,1,1,0,0,1,0-1.42l6-6a1,1,0,0,1,1.42,1.42Z"
+                style="fill:#fff"
+              ></path>
+            </g>
+          </svg>
+        </div>
+        <span className={`cart-toast__reward-tier-text`}>
+          {cartTotalPrice > minPrice && cartTotalPrice < maxPrice ? (
+            tierLevel
+          ) : (
+            <>
+              <span className={`cart-toast__reward-tier-text-boost`}>
+                <svg width="15px" height="15px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                  <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                  <g id="SVGRepo_iconCarrier">
+                    {' '}
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12ZM16.0303 8.96967C16.3232 9.26256 16.3232 9.73744 16.0303 10.0303L11.0303 15.0303C10.7374 15.3232 10.2626 15.3232 9.96967 15.0303L7.96967 13.0303C7.67678 12.7374 7.67678 12.2626 7.96967 11.9697C8.26256 11.6768 8.73744 11.6768 9.03033 11.9697L10.5 13.4393L12.7348 11.2045L14.9697 8.96967C15.2626 8.67678 15.7374 8.67678 16.0303 8.96967Z"
+                      fill="#de9151"
+                    ></path>{' '}
+                  </g>
+                </svg>
+              </span>
+              <span className={`cart-toast__reward-tier-text-span`}>{tierLevel}</span>
+            </>
+          )}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+type RewardTierType = {
+  tierLevel: string;
+  minPrice: number;
+  maxPrice?: number;
+};
+
 const ProgressBar = ({ cartTotalPrice, maxPrice }: { cartTotalPrice: number; maxPrice: number }) => {
   const progress = useCallback(() => Math.max(0, Math.min(100, (cartTotalPrice / maxPrice) * 100)), [cartTotalPrice]);
+  const rewardTiers: RewardTierType[] = [
+    {
+      tierLevel: '5% off!',
+      minPrice: 0,
+      maxPrice: 18,
+    },
+    {
+      tierLevel: '10% off!',
+      minPrice: 18,
+      maxPrice: 36,
+    },
+    {
+      tierLevel: '15% off!',
+      minPrice: 36,
+      maxPrice: 54,
+    },
+    {
+      tierLevel: '20% off!',
+      minPrice: 54,
+    },
+  ];
   return (
     <>
-      <div className={`cart-toast__progress-bar`}>
-        <div className={`cart-toast__progress-bar-fill`} style={`display: inline-block; width: ${progress()}%;`}></div>
+      <div className={`cart-toast__progress-bar-wrapper`}>
+        <div className={`cart-toast__progress-bar`}>
+          <div
+            className={`cart-toast__progress-bar-fill`}
+            style={`display: inline-block; width: calc(${progress()}% + 1px);`}
+          ></div>
+        </div>
+
+        <div className={`cart-toast__tier-bar`}>
+          {rewardTiers.map((tier, index) => {
+            return (
+              <>
+                <RewardTier
+                  key={index}
+                  tierLevel={tier.tierLevel}
+                  minPrice={tier.minPrice}
+                  maxPrice={tier.maxPrice}
+                  cartTotalPrice={cartTotalPrice}
+                />
+              </>
+            );
+          })}
+        </div>
       </div>
     </>
   );
--- src/cart-toast/main.tsx
+++ src/cart-toast/main.tsx
@@ -3,6 +3,6 @@ import { CartToast } from './app.tsx';
 
 const cartToast: HTMLElement | null = document.getElementById('cart-toast');
 let cartTotalPrice: number | undefined = parseInt(cartToast?.dataset.totalPrice || '0', 10);
-const cartCurrency: string | undefined = cartToast?.dataset.currency?.charAt(0);
+const cartCurrency: string | undefined = cartToast?.dataset.currency?.charAt(0) || "$";
 
 render(<CartToast cartTotalPrice={cartTotalPrice} cartCurrency={cartCurrency} />, cartToast!);
